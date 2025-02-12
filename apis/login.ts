27bd25f01eda3f2e9ab9a3eb86973c6e69b7c8c6
sonld <son.ld24699@gmail.com>
2025-02-12 07:30:44 +0700

    login util

--- apis/login.ts
+++ apis/login.ts
@@ -1,17 +1,103 @@
 import { ContextBase } from "../src/context";
-import { makeURL, request } from "../src/utils";
+import { decryptResp, makeURL, ParamsEncryptor, request } from "../src/utils";
+import * as cryptojs from 'crypto-js';
 
 export async function login(ctx: ContextBase, encryptParams: boolean) {
-    const response = await request(
-        ctx,
-        makeURL(ctx, "https://wpa.chat.zalo.me/api/login/getLoginInfo", {
-            // ...encryptedParams.params,
-            nretry: 0,
-        }),
-    );
+    const encryptedParams = await getEncryptParam(ctx, encryptParams, "getlogininfo");
+    try {
+        const response = await request(
+            ctx,
+            makeURL(ctx, "https://wpa.chat.zalo.me/api/login/getLoginInfo", {
+                ...encryptedParams.params,
+                nretry: 0,
+            }),
+        );
+    
+        const data = await response.json();
+        if (encryptedParams.enk) {
+            const decryptedData = decryptResp(encryptedParams.enk, data.data);
+            return decryptedData != null && typeof decryptedData != "string" ? decryptedData : null;
+        }
+    
+        return null;
+    } catch (error) {
+        console.log(error);
+        throw new Error("Failed to login: " + error);
+    }
+}
 
-    const data = await response.json();
-    console.log(data);
-    
-    
+export function getSignKey(type: string, params: Record<string, any>) {
+    let n = [];
+    for (let s in params) {
+        if (params.hasOwnProperty(s)) {
+            n.push(s);
+        }
+    }
+
+    n.sort();
+    let a = "zsecure" + type;
+    for (let s = 0; s < n.length; s++) a += params[n[s]];
+    return cryptojs.MD5(a);
+}
+
+async function getEncryptParam(ctx: ContextBase, encryptParams: boolean, type: string) {
+    const params = {} as Record<string, any>;
+    const data = {
+        computer_name: "Web",
+        imei: ctx.imei!,
+        language: ctx.language!,
+        ts: Date.now(),
+    };
+    const encryptedData = await _encryptParam(ctx, data, encryptParams);
+
+    if (encryptedData == null) Object.assign(params, data);
+    else {
+        const { encrypted_params, encrypted_data } = encryptedData;
+        Object.assign(params, encrypted_params);
+        params.params = encrypted_data;
+    }
+
+    params.type = ctx.API_TYPE;
+    params.client_version = ctx.API_VERSION;
+    params.signkey =
+        type == "getserverinfo"
+            ? getSignKey(type, {
+                  imei: ctx.imei,
+                  type: ctx.API_TYPE,
+                  client_version: ctx.API_VERSION,
+                  computer_name: "Web",
+              })
+            : getSignKey(type, params);
+
+    return {
+        params,
+        enk: encryptedData ? encryptedData.enk : null,
+    };
+}
+
+async function _encryptParam(ctx: ContextBase, data: Record<string, any>, encryptParams: boolean) {
+    if (encryptParams) {
+        const encryptor = new ParamsEncryptor({
+            type: ctx.API_TYPE,
+            imei: data.imei,
+            firstLaunchTime: Date.now(),
+        });
+        try {
+            const stringifiedData = JSON.stringify(data);
+            const encryptedKey = encryptor.getEncryptKey();
+            const encodedData = ParamsEncryptor.encodeAES(encryptedKey, stringifiedData, "base64", false);
+            const params = encryptor.getParams();
+
+            return params
+                ? {
+                      encrypted_data: encodedData,
+                      encrypted_params: params,
+                      enk: encryptedKey,
+                  }
+                : null;
+        } catch (error) {
+            throw new Error("Failed to encrypt params: " + error);
+        }
+    }
+    return null;
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,7 @@
 import { ContextBase } from "./context";
 import * as toughCookie from 'tough-cookie';
+import cryptojs from "crypto-js";
+import { ZaloApiError } from "./errors/ZaloApiErros";
 
 
 export const isBun = typeof Bun !== "undefined";
@@ -87,4 +89,146 @@ export async function getDefaultHeaders(ctx: ContextBase, origin: string = "http
         Referer: "https://chat.zalo.me/",
         "User-Agent": ctx.userAgent,
     };
+}
+
+export class ParamsEncryptor {
+    private zcid: string | null = null;
+    private enc_ver: string;
+    private zcid_ext: string;
+    private encryptKey: string | null;
+    constructor({ type, imei, firstLaunchTime }: { type: number; imei: string; firstLaunchTime: number }) {
+        this.enc_ver = "v2";
+        this.zcid = null;
+        this.encryptKey = null;
+
+        this.createZcid(type, imei, firstLaunchTime);
+        this.zcid_ext = ParamsEncryptor.randomString();
+        this.createEncryptKey();
+    }
+
+    getEncryptKey() {
+        if (!this.encryptKey) throw new ZaloApiError("getEncryptKey: didn't create encryptKey yet");
+        return this.encryptKey;
+    }
+
+    createZcid(type: number, imei: string, firstLaunchTime: number) {
+        if (!type || !imei || !firstLaunchTime) throw new ZaloApiError("createZcid: missing params");
+        const msg = `${type},${imei},${firstLaunchTime}`;
+        const s = ParamsEncryptor.encodeAES("3FC4F0D2AB50057BCE0D90D9187A22B1", msg, "hex", true);
+        this.zcid = s;
+    }
+
+    createEncryptKey(e = 0) {
+        const t = (e: string, t: string) => {
+            const { even: n } = ParamsEncryptor.processStr(e),
+                { even: a, odd: s } = ParamsEncryptor.processStr(t);
+            if (!n || !a || !s) return !1;
+            const i = n.slice(0, 8).join("") + a.slice(0, 12).join("") + s.reverse().slice(0, 12).join("");
+            return (this.encryptKey = i), !0;
+        };
+        if (!this.zcid || !this.zcid_ext) throw new ZaloApiError("createEncryptKey: zcid or zcid_ext is null");
+        try {
+            let n = cryptojs.MD5(this.zcid_ext).toString().toUpperCase();
+            if (t(n, this.zcid) || !(e < 3)) return !1;
+            this.createEncryptKey(e + 1);
+        } catch (n) {
+            e < 3 && this.createEncryptKey(e + 1);
+        }
+        return !0;
+    }
+
+    getParams() {
+        return this.zcid
+            ? {
+                  zcid: this.zcid,
+                  zcid_ext: this.zcid_ext,
+                  enc_ver: this.enc_ver,
+              }
+            : null;
+    }
+
+    static processStr(e: string) {
+        if (!e || "string" != typeof e)
+            return {
+                even: null,
+                odd: null,
+            };
+        const [t, n] = [...e].reduce((e, t, n) => (e[n % 2].push(t), e), [[], []] as string[][]);
+        return {
+            even: t,
+            odd: n,
+        };
+    }
+
+    static randomString(e?: number, t?: number) {
+        const n = e || 6,
+            a = t && e && t > e ? t : 12;
+        let s = Math.floor(Math.random() * (a - n + 1)) + n;
+        if (s > 12) {
+            let e = "";
+            for (; s > 0; )
+                (e += Math.random()
+                    .toString(16)
+                    .substr(2, s > 12 ? 12 : s)),
+                    (s -= 12);
+            return e;
+        }
+        return Math.random().toString(16).substr(2, s);
+    }
+
+    static encodeAES(e: string, message: string, type: "hex" | "base64", uppercase: boolean, s = 0): string | null {
+        if (!message) return null;
+        try {
+            {
+                const encoder = "hex" == type ? cryptojs.enc.Hex : cryptojs.enc.Base64;
+                const key = cryptojs.enc.Utf8.parse(e);
+
+                const cfg = {
+                    words: [0, 0, 0, 0],
+                    sigBytes: 16,
+                } as cryptojs.lib.WordArray;
+                const encrypted = cryptojs.AES.encrypt(message, key, {
+                    iv: cfg,
+                    mode: cryptojs.mode.CBC,
+                    padding: cryptojs.pad.Pkcs7,
+                }).ciphertext.toString(encoder);
+
+                return uppercase ? encrypted.toUpperCase() : encrypted;
+            }
+        } catch (o) {
+            return s < 3 ? ParamsEncryptor.encodeAES(e, message, type, uppercase, s + 1) : null;
+        }
+    }
+}
+
+export function decryptResp(key: string, data: string): Record<string, any> | null | string {
+    let n = null;
+    try {
+        n = decodeRespAES(key, data);
+        const parsed = JSON.parse(n);
+        return parsed;
+    } catch (error) {
+        return n;
+    }
+}
+
+function decodeRespAES(key: string, data: string) {
+    data = decodeURIComponent(data);
+    const parsedKey = cryptojs.enc.Utf8.parse(key);
+    const n = {
+        words: [0, 0, 0, 0],
+        sigBytes: 16,
+    } as cryptojs.lib.WordArray;
+
+    return cryptojs.AES.decrypt(
+        {
+            ciphertext: cryptojs.enc.Base64.parse(data),
+        } as cryptojs.lib.CipherParams,
+        parsedKey,
+        {
+            iv: n,
+            mode: cryptojs.mode.CBC,
+            padding: cryptojs.pad.Pkcs7,
+        },
+    ).toString(cryptojs.enc.Utf8);
 }
